2c02310a65fa664eb873d5eb8d3e5ce216b30700
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 04:47:22 +0000

    Adding numeric_owner as keyword arguement

--- pyiron_base/objects/job/core.py
+++ pyiron_base/objects/job/core.py
@@ -837,7 +837,7 @@ class JobCore(PyIronObject):
                         if not is_within_directory(path, member_path):
                             raise Exception("Attempted Path Traversal in Tar File")
                 
-                    tar.extractall(path, members, numeric_owner) 
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
                     
                 
                 safe_extract(tar, self.working_directory)
@@ -895,7 +895,7 @@ class JobCore(PyIronObject):
                         if not is_within_directory(path, member_path):
                             raise Exception("Attempted Path Traversal in Tar File")
                 
-                    tar.extractall(path, members, numeric_owner) 
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
                     
                 
                 safe_extract(tar, fpath)
